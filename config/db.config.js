e8f33ae6d54f68a47a2d80742ae10d674edc22aa
HUGO ANAYA <hugo.anaya@live.com>
2022-06-08 13:06:54 -0500

    db config

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "mascotas"
-}
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "b2aaf645739468",
+  PASSWORD: "b9bb458b",
+  DB: "heroku_45164af6e2b199c",
+};
